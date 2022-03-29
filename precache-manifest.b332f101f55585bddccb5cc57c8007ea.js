59b9df38bd4b70f91eac8d64a8f2bdc0edf5ab2b
Ticha <tichagodwill1@gmail.com>
2022-03-29 14:48:29 +0300

    Updates

---
+++ precache-manifest.b332f101f55585bddccb5cc57c8007ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee3fa3b5b0d87686a905",
+    "url": "/RoboFriends/static/css/main.da0c7271.chunk.css"
+  },
+  {
+    "revision": "ee3fa3b5b0d87686a905",
+    "url": "/RoboFriends/static/js/main.ee3fa3b5.chunk.js"
+  },
+  {
+    "revision": "c4a863e1ef9e146c2113",
+    "url": "/RoboFriends/static/css/1.d63f0d99.chunk.css"
+  },
+  {
+    "revision": "c4a863e1ef9e146c2113",
+    "url": "/RoboFriends/static/js/1.c4a863e1.chunk.js"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "fd82cb04c66eb2dc209349b89f176e3d",
+    "url": "/RoboFriends/index.html"
+  }
+];
